a60f4511d4d90a215b9d1b8d443d404eb6f07744
Артем Храбан <a.chastener@gmail.com>
2020-08-01 12:32:22 +0300

    db rollback

--- methods.py
+++ methods.py
@@ -1,19 +1,34 @@
 from classes import *
 
 def set_image_name(image_id, image_name):
-    session.query(Image).filter(Image.id == image_id). \
-        update({Image.image_name: image_name},  synchronize_session=False)
-    session.commit()
+    try:
+        session.query(Image).filter(Image.id == image_id). \
+            update({Image.image_name: image_name},  synchronize_session=False)
+        session.commit()
+    except Exception as ex:
+        print(ex)
+        session.rollback()
+        return
 
 
 def create_image_instance(user_id, image_url, thumb_url):
-    instance = Image(user_id=user_id, image_url=image_url, thumb_url=thumb_url)
-    session.add(instance)
-    session.commit()
+    try:
+        instance = Image(user_id=user_id, image_url=image_url, thumb_url=thumb_url)
+        session.add(instance)
+        session.commit()
 
-    image_id = session.query(Image).filter(Image.image_url == image_url).all()
-    return image_id[0].id
+        image_id = session.query(Image).filter(Image.image_url == image_url).all()
+        return image_id[0].id
+    except Exception as ex:
+        print(ex)
+        session.rollback()
+        return
 
 def get_image_by_name(user_id, user_input):
-    instance = session.query(Image).filter(Image.user_id == user_id, Image.image_name.like(f'%{user_input}%')).all()
-    return instance
+    try:
+        instance = session.query(Image).filter(Image.user_id == user_id, Image.image_name.like(f'%{user_input}%')).all()
+        return instance
+    except Exception as ex:
+        print(ex)
+        session.rollback()
+        return
